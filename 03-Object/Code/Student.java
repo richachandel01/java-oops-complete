be3706cc7d710c9ee6715f698f5daeb18db4e98a
richachandel01 <chandelricha24@gmail.com>
2026-08-05 11:24:30 +0530

    feat(object): demonstrate object creation and usage

---
+++ 03-Object/Code/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    String name;
+    int age;
+
+    void display() {
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println();
+
+    }
+
+}
