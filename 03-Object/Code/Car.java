ef43adda4d606490965004b35a04e38dd92b9783
richachandel01 <chandelricha24@gmail.com>
2026-08-05 11:24:36 +0530

    feat(object): add real-world object examples

---
+++ 03-Object/Code/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    String brand;
+    int speed;
+
+    void showDetails() {
+
+        System.out.println("Brand : " + brand);
+        System.out.println("Speed : " + speed);
+
+    }
+
+}
---
+++ 03-Object/Code/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+    int id;
+    String name;
+
+    void showEmployee() {
+
+        System.out.println(id);
+        System.out.println(name);
+
+    }
+
+}
