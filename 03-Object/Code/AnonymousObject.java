d104569441c4ca8e39f7a1588854e12eb0a97ed5
richachandel01 <chandelricha24@gmail.com>
2026-08-05 11:24:33 +0530

    feat(object): add anonymous object example

---
+++ 03-Object/Code/AnonymousObject.java
@@ -0,0 +1,19 @@
+class Demo {
+
+    void show() {
+
+        System.out.println("Anonymous Object");
+
+    }
+
+}
+
+public class AnonymousObject {
+
+    public static void main(String[] args) {
+
+        new Demo().show();
+
+    }
+
+}
