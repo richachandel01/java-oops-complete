8abaeee51fe72dc0696409e8845d2ddccef14440
richachandel01 <chandelricha24@gmail.com>
2026-08-05 11:26:51 +0530

    feat(object): add remaining object examples

--- 03-Object/Code/ArrayOfObjects.java
+++ 03-Object/Code/ArrayOfObjects.java
@@ -1,28 +1,5 @@
+package Object;
+
 public class ArrayOfObjects {
-
-    public static void main(String[] args) {
-
-        Student[] students = new Student[3];
-
-        students[0] = new Student();
-        students[1] = new Student();
-        students[2] = new Student();
-
-        students[0].name = "Aman";
-        students[0].age = 20;
-
-        students[1].name = "Richa";
-        students[1].age = 22;
-
-        students[2].name = "Karan";
-        students[2].age = 21;
-
-        for (Student student : students) {
-
-            student.display();
-
-        }
-
-    }
-
+    
 }
---
+++ 03-Object/Code/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Richa";
+        s1.age = 22;
+
+        Student s2 = new Student();
+
+        s2.name = "Rahul";
+        s2.age = 21;
+
+        s1.display();
+        s2.display();
+
+    }
+
+}
